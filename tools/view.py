b4b2aea26fad1120b74d339d71f5da2e6b692c69
yy981-jp <134594870+yy981-jp@users.noreply.github.com>
2026-04-18 16:51:37 +0900

    fix: グラフの言葉をより正確な言葉に

--- tools/view.py
+++ tools/view.py
@@ -1,6 +1,5 @@
 import json
 import matplotlib.pyplot as plt
-from matplotlib import font_manager
 
 # JSON読み込み
 with open('../build/data.json', 'r', encoding='utf-8') as f:
@@ -8,7 +7,9 @@ with open('../build/data.json', 'r', encoding='utf-8') as f:
 
 
 plt.rcParams['font.family'] = 'Meiryo'
+plt.tight_layout()
 
+# ===== 個別に出力 =====
 for op, content in data.items():
     plt.figure()
 
@@ -16,19 +17,57 @@ for op, content in data.items():
     if "dep" in content:
         x_dep = list(map(int, content["dep"].keys()))
         y_dep = list(content["dep"].values())
-        plt.plot(x_dep, y_dep, marker='o', label="直列処理")
+        plt.plot(x_dep, y_dep, marker='o', label="データ依存処理")
 
     # ind
     if "ind" in content:
         x_ind = list(map(int, content["ind"].keys()))
         y_ind = list(content["ind"].values())
-        plt.plot(x_ind, y_ind, marker='o', label="並列処理")
+        plt.plot(x_ind, y_ind, marker='o', label="独立処理")
 
     plt.title(op.upper())
     plt.xlabel("命令数")
-    plt.ylabel("所要cycle")
+    plt.ylabel("所要時間 (cycles)")
     plt.legend()
     plt.grid()
 
     # plt.show()
     plt.savefig(f"graphs/{op}.png", dpi=300)
+    plt.close()
+
+# ===== dep 全opまとめ =====
+plt.figure()
+
+for op, content in data.items():
+    if "dep" in content:
+        x = list(map(int, content["dep"].keys()))
+        y = list(content["dep"].values())
+        plt.plot(x, y, marker='o', label=op)
+
+plt.title("データ依存処理")
+plt.xlabel("命令数")
+plt.ylabel("所要時間 (cycles)")
+plt.legend()
+plt.grid()
+
+plt.savefig("graphs/_dep.png", dpi=300)
+plt.close()
+
+
+# ===== ind 全opまとめ =====
+plt.figure()
+
+for op, content in data.items():
+    if "ind" in content:
+        x = list(map(int, content["ind"].keys()))
+        y = list(content["ind"].values())
+        plt.plot(x, y, marker='o', label=op)
+
+plt.title("独立処理")
+plt.xlabel("命令数")
+plt.ylabel("所要時間 (cycles)")
+plt.legend()
+plt.grid()
+
+plt.savefig("graphs/_ind.png", dpi=300)
+plt.close()
